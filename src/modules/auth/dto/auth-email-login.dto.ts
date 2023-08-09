d4926dcd607613d5a5bb39914a40402b71f31303
Thuong To <thuongthanhto@gmail.com>
2023-08-09 11:54:10 +0700

    Update

--- src/modules/auth/dto/auth-email-login.dto.ts
+++ src/modules/auth/dto/auth-email-login.dto.ts
@@ -1,6 +1,6 @@
 import { IsNotEmpty } from 'class-validator';
 import { Transform } from 'class-transformer';
-import { lowerCaseTransformer } from 'src/core/utils/transformers/lower-case.transformer';
+import { lowerCaseTransformer } from '../../../core/utils/transformers/lower-case.transformer';
 
 export class AuthEmailLoginDto {
   @Transform(lowerCaseTransformer)
