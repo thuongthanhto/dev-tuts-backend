446b4337fc39e1c78fce4a592fd7c558651796fb
Thuong To <thuongthanhto@gmail.com>
2024-06-27 22:47:29 +0700

    Add html file

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
@@ -19,6 +21,9 @@ import { SessionModule } from '../session/session.module';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../..', 'public'),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [
--- src/modules/app/controllers/app.controller.ts
+++ src/modules/app/controllers/app.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { join } from 'path';
+
 import { AppService } from '../services/app.service';
 
 @Controller()
@@ -6,7 +9,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Res() res: Response): void {
+    return res.sendFile(join(`${process.cwd()}/public/index.html`));
   }
 }
